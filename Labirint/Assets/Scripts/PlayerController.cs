03d43914b2f6bb6c8d1931de776d2236172762a5
Zvonimir Vlaić <mali.bluste@gmail.com>
2024-10-22 20:56:37 +0200

    Nakon treceg predavanja

--- Labirint/Assets/Scripts/PlayerController.cs
+++ Labirint/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 10f;
+    [SerializeField] float speed = 12f;
     Vector3 velocity;
     CharacterController characterController;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,24 @@ public class PlayerController : MonoBehaviour
 
     void PlayerMove()
     {
+        RaycastHit hit;
+        if (Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out hit, 1f, groundMask)) {
+            string terrainType;
+            terrainType = hit.collider.gameObject.tag;
+            switch (terrainType) {
+                default:
+                    speed = 12f;
+                    break;
+                case "Low":
+                    speed = 3f;
+                    break;
+                case "High":
+                    speed = 20f;
+                    break;
+            }
+        }
+
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
