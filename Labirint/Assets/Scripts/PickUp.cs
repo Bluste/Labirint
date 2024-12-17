e6c3c8fa7d38562717d2e5d568bcad978d75d8f3
Zvonimir Vlaić <mali.bluste@gmail.com>
2024-12-17 21:20:12 +0100

    Nakon 10. lekcije

--- Labirint/Assets/Scripts/PickUp.cs
+++ Labirint/Assets/Scripts/PickUp.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum RotationAxis { 
+    x,y,z
+}
 public class PickUp : MonoBehaviour
 {
+    public RotationAxis rotationAxis;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +26,20 @@ public class PickUp : MonoBehaviour
     }
 
     public void Rotation() {
-        transform.Rotate(new Vector3(0f, 2f, 0f));
+        switch (rotationAxis)
+        {
+            case RotationAxis.x:
+                transform.Rotate(new Vector3(1f, 0f, 0f));
+                break;
+            case RotationAxis.y:
+                transform.Rotate(new Vector3(0f, 1f, 0f));
+                break;
+            case RotationAxis.z:
+                transform.Rotate(new Vector3(0f, 0f, 1f));
+                break;
+            default:
+                break;
+        }
+        
     }
 }
